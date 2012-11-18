f99ccca5eccc31b8eac9da4d6c47c8d19bcb3191
Sebastien Vincent <seb.vincent@gmail.com>
2012-11-18 19:58:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# grunt-contrib-manifest
+
+manifest task ported from Yeoman
+
+## Getting Started
+Install this grunt plugin next to your project's [Gruntfile][getting_started] with: `npm install grunt-contrib-manifest`
+
+Then add this line to your project's Gruntfile:
+
+```javascript
+grunt.loadNpmTasks('grunt-contrib-manifest');
+```
+
+[grunt]: https://github.com/cowboy/grunt
+[getting_started]: https://github.com/cowboy/grunt/blob/master/docs/getting_started.md
+
+## Documentation
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt][grunt].
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2012 Sebastien Vincent  
+Licensed under the MIT license.
---
+++ tasks/contrib-manifest.js
@@ -0,0 +1,87 @@
+/*
+ * grunt-contrib-manifest
+ * Licensed under the MIT license.
+ */
+
+var url     = require('url');
+var path    = require('path');
+var join    = path.join;
+var fstream = require('fstream');
+
+module.exports = function(grunt) {
+  
+  var helpers = require('../lib/manifest-helpers')(grunt); 
+
+  // Launch a built-in webserver on the specified directory and run confess through phantomjs.
+  //
+  // This task can be used to run other confess task like performance or css,
+  // and can be configured to operate on a specific subdirectory.
+  //
+  // - task   - first task argument is used to specify the confess task to run
+  //            (default: appcache)
+  // - basdir - base directory of the app to cache (default: './')
+  //
+  // Examples
+  //
+  //    grunt manifest
+  //    grunt manifest:appcache:path/to/my/app
+  //    grunt manifest:performance:app
+  //    grunt manifest:performance:temp
+  //
+  // Advanced options can be configured below for expected options. Can be configures in the application gruntfile.
+  //
+  // Examples
+  //
+  //    manifest: {
+  //      task: 'appcache',
+  //      output: 'manifest.appcache',
+  //      port: 3501,
+  //      hostname: 'localhost',
+  //      base: 'app/'
+  //    }
+  //
+  //
+  grunt.registerTask('manifest', 'Generates an application cache manifest using Confess.js.', function() {
+    // port of the url to cache
+    var port = grunt.config('server.port') || 3501;
+
+    // default options
+    var options = grunt.util._.defaults(grunt.config(this.name) || {}, {
+      // confess task, set via first grunt task arg (default: appcache)
+      // performance, appcache, cssproperties
+      task: this.args[0] || 'appcache',
+      // output file
+      output: 'manifest.appcache',
+      // port of the url to cache,
+      port: port,
+      // hostname of the url to cache
+      hostname: 'localhost',
+      // basedir
+      base: path.resolve(this.args[1] || './'),
+      // if the browser should be "opened" to the app to cache
+      open: false,
+      // if the reload snippet from livereload should be injected or not
+      inject: false
+    });
+
+    // Tell grunt this task is asynchronous.
+    var done = this.async();
+
+    // start a webserver at the specified location
+    //start a server before task
+    /*
+    grunt.helper('server', options, function(err, port) {
+      if(err) {
+        return done( err );
+      }
+
+      options.port = port;
+      grunt.helper('manifest', options, done);
+    });
+    */
+    //options.port = port;
+    helpers.manifest(options, done);
+  });
+
+};
+
